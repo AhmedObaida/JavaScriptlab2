43ad99d7142632aa3134bc71faabb003a6fda2c2
ahmed <ahmed@gmail.com>
2022-12-03 23:54:08 +0200

    add

--- taks1.js
+++
@@ -1,8 +0,0 @@
-// alert("Watch up!");
-// confirm("Are you Male ?");
-// prompt("enter YOur name");
-// document.write(!! 123);
-var myBoolean=new Boolean(null);
-document.write(myBoolean);
-var myDate = new Date();
-document.write("<br>"+myDate);
